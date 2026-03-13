e9bd89c55dacf38995818d5c93bf2255130d78cc
shenron612 <wanyamaelisha612@gmail.com>
2026-03-13 15:23:25 +0300

    first commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/emobilis/Downloads/hirelink/venv
+set -gx VIRTUAL_ENV /home/emobilis/Downloads/backup/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
